866861cce17396278dabb403efc8d05ecf45f6c7
Vitaly Kondratev <kvl.2000@mail.ru>
2023-12-09 18:30:02 +0300

    Initial commit

---
+++ MPI_SQL_Coursework/DTOs/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MPI_SQL_Coursework.DTOs
+{
+    internal class Product
+    {
+    }
+}
